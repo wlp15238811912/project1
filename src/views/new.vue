fa384859e5feef9bdf905e7288d60ea3051dd7aa
wanglipan <1662880176@qq.com>
2022-09-27 15:32:10 +0800

    new新文件添加

---
+++ src/views/new.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<h1>新文件添加</h1>
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   created(){
+   },
+   computed:{
+   },
+   methods:{
+   },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
